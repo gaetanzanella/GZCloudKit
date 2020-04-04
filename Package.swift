71bac57a96c22702b00182bf5e26ea3ba84055d9
Gaétan Zanella <gaetan.zanella@fabernovel.com>
2020-04-04 16:40:53 +0200

    Define supported platforms

--- Package.swift
+++ Package.swift
@@ -5,11 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "GZCloudKit",
+    platforms: [
+        .macOS(.v10_14),
+        .iOS(.v10)
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "GZCloudKit",
-            targets: ["GZCloudKit"]),
+            targets: ["GZCloudKit"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
